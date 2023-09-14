feddcf5e890c14e86fe22f1f00050d85559c081f
yeongjunekoh <rhdudwns0@naver.com>
2023-09-14 12:09:25 +0900

    [FEATURE] Generate main route constructure

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  createStackNavigator,
+  StackNavigationOptions,
+} from '@react-navigation/stack';
+
+import {RootScreenStackPropsList, ROUTER} from '../config';
+import MainStackScreen from './main';
+
+const RootStack = createStackNavigator<RootScreenStackPropsList>();
 
 function RouterScreen(): React.ReactElement {
-  return <></>;
+  const headerShownOptions: StackNavigationOptions = {
+    headerShown: false,
+    headerTitleAlign: 'center',
+  };
+
+  return (
+    <RootStack.Navigator>
+      <RootStack.Screen
+        name={ROUTER.MAIN_SCREEN}
+        component={MainStackScreen}
+        options={headerShownOptions}
+      />
+    </RootStack.Navigator>
+  );
 }
 
 export default RouterScreen;
