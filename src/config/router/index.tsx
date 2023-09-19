c860f3a724a9482814473549bb83a3c565d78a93
yeongjunekoh <rhdudwns0@naver.com>
2023-09-20 03:58:42 +0900

    [FEATURE] Generate structure challenge apply

--- src/config/router/index.tsx
+++ src/config/router/index.tsx
@@ -6,6 +6,8 @@ export enum ROUTER {
   STORE_SCREEN = 'STORE_SCREEN',
   PARTICIPATION_SCREEN = 'PARTICIPATION_SCREEN',
   PROFILE_SCREEN = 'PROFILE_SCREEN',
+  CHALLENGE_EXPLAIN_SCREEN = 'CHALLENGE_EXPLAIN_SCREEN',
+  CHALLENGE_APPLY_SCREEN = 'CHALLENGE_APPLY_SCREEN',
 }
 
 // 최상단 RootScreen
@@ -17,15 +19,17 @@ export type RootScreenStackPropsList = {
 };
 
 // MainScreen을 Navigator에 내부 Screen
-// 상위 RootScreen 포함한다.
+// RootScreen에서 이동했기에 RootScreen을 포함한다.
 export type MainScreenKeyList = keyof MainScreenStackPropsList;
 
 export type MainScreenStackPropsList = RootScreenStackPropsList & {
   [ROUTER.HOME_SCREEN]: undefined;
+  [ROUTER.CHALLENGE_EXPLAIN_SCREEN]: undefined;
+  [ROUTER.CHALLENGE_APPLY_SCREEN]: undefined;
 };
 
 // HomeScreen Navigator에 내부 Screen
-// 상위 MainScreen을 포함한다.
+// MainScreen에서 이동했기에 MainScreen을 포함한다.
 export type HomeScreenKeyList = keyof HomeScreenStackPropsList;
 
 export type HomeScreenStackPropsList = MainScreenStackPropsList & {
--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -5,7 +5,10 @@ import {
 } from '@react-navigation/stack';
 
 import {ROUTER, MainScreenStackPropsList} from '../../config';
+
 import HomeScreen from './HomeScreen';
+import ChallengeExplainScreen from './ChallengeExplainScreen';
+import ChallengeApplyScreen from './ChallengeApplyScreen';
 
 const MainStack = createStackNavigator<MainScreenStackPropsList>();
 
@@ -19,6 +22,16 @@ function MainStackScreen(): React.ReactElement {
       <MainStack.Screen
         name={ROUTER.HOME_SCREEN}
         component={HomeScreen}
+        options={headerHideOptions}
+      />
+      <MainStack.Screen
+        name={ROUTER.CHALLENGE_EXPLAIN_SCREEN}
+        component={ChallengeExplainScreen}
+        options={headerHideOptions}
+      />
+      <MainStack.Screen
+        name={ROUTER.CHALLENGE_APPLY_SCREEN}
+        component={ChallengeApplyScreen}
         options={headerHideOptions}
       />
     </MainStack.Navigator>
