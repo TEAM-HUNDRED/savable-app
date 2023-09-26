7039cde319e9068d3152306fabb6f22ad9c6d9e4
yeongjunekoh <rhdudwns0@naver.com>
2023-09-27 02:04:58 +0900

    [FEATURE] Genrate create order input container

--- src/screens/main/CreateOrderPage/index.tsx
+++ src/screens/main/CreateOrderPage/index.tsx
@@ -1,18 +1,41 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {RouteProp, useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import React, {useEffect} from 'react';
-import {ScrollView, StyleSheet, View} from 'react-native';
 
 import GiftCard from '../../../components/card/GiftCard';
 import {AppStyles, MainScreenStackPropsList, ROUTER} from '../../../config';
+import CreateOrderContainer from '../../../container/CreateOrderContainer';
+import {useToastProvider} from '../../../lib/context/ToastContext';
 
 type PropsType = {
   route: RouteProp<MainScreenStackPropsList, ROUTER.CREATE_ORDER_PAGE>;
 };
 
+type InputPropsType = {
+  amount: number;
+  negativePoint: string;
+  positivePoint: string;
+  challengeOpinion: string;
+};
+
 function CreateOrderPage({route}: PropsType) {
   const navigation =
     useNavigation<StackNavigationProp<MainScreenStackPropsList>>();
+  const {showToast} = useToastProvider();
+
+  const [inputData, setInputData] = useState<InputPropsType>({
+    amount: 1,
+    negativePoint: '',
+    positivePoint: '',
+    challengeOpinion: '',
+  });
+
+  console.log(inputData);
+
+  const handleInputData = useCallback((currentInputData: InputPropsType) => {
+    setInputData(currentInputData);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({title: '기프티콘 구매'});
@@ -29,6 +52,10 @@ function CreateOrderPage({route}: PropsType) {
           productName={route.params.productName}
           price={route.params.price}
           brandName={route.params.brandName}
+        />
+        <CreateOrderContainer
+          inputData={inputData}
+          handleInputData={handleInputData}
         />
       </View>
     </ScrollView>
