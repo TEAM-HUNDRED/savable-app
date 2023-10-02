bef1430a8576bb8632ccfdaaa35e679121d10309
yeongjunekoh <rhdudwns0@naver.com>
2023-10-03 01:43:31 +0900

    [FEATURE] Generate user reward info component

--- src/screens/main/HomeScreen/ProfileTabScreen/index.tsx
+++ src/screens/main/HomeScreen/ProfileTabScreen/index.tsx
@@ -1,29 +1,23 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, View} from 'react-native';
-import {RouteProp, useNavigation} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
+import {Image, ScrollView, StyleSheet, View} from 'react-native';
 
-import {
-  AppStyles,
-  HomeScreenStackPropsList,
-  MainScreenStackPropsList,
-  ROUTER,
-} from '../../../../config';
-import {CheckIcon} from '../../../../assets/icons';
+import {AppStyles} from '../../../../config';
+import Icons from '../../../../assets/icons';
 
-import SVText from '../../../../components/common/SVText';
-import SVDivider from '../../../../components/common/SVDivider';
-import SVButton from '../../../../components/common/SVButton';
 import Api from '../../../../lib/api/Api';
 import {
   UserChallengeInfoPropsType,
   UserRewardInfoPropsType,
 } from '../../../../types/view';
 
+import SVText from '../../../../components/common/SVText';
+
 type PropsType = {};
 
 function ProfileTabScreen({}: PropsType) {
-  const [userInfo, setUserInfo] = useState<UserRewardInfoPropsType>();
+  const [userInfo, setUserInfo] = useState<UserRewardInfoPropsType>(
+    {} as UserRewardInfoPropsType,
+  );
   const [userChallengeInfo, setUserChallengeInfo] =
     useState<UserChallengeInfoPropsType>();
 
@@ -38,6 +32,23 @@ function ProfileTabScreen({}: PropsType) {
     }
   };
 
+  const userRewardInfoList = [
+    {
+      index: '포인트',
+      value: `${userInfo.totalReward}포인트`,
+      icon: Icons.point,
+      description: `${userInfo.scheduledReward}포인트`,
+    },
+    {
+      index: '총 절약 금액',
+      value: `${userInfo.totalSavings}원`,
+    },
+    {
+      index: '인증 횟수',
+      value: `${userInfo.verificationCount}회`,
+    },
+  ];
+
   useEffect(() => {
     getUserInfo();
   }, []);
@@ -46,6 +57,35 @@ function ProfileTabScreen({}: PropsType) {
     <ScrollView style={styles.container}>
       <View style={styles.paddingContainer}>
         <View style={styles.profileContainer}></View>
+        <View style={styles.statusContainer}>
+          {userRewardInfoList.map((item, idx) => {
+            const isLastItem = idx === userRewardInfoList.length - 1;
+
+            return (
+              <>
+                <View style={styles.barContainer}>
+                  <SVText body08>{item.index}</SVText>
+                  <View>
+                    <View style={styles.valueContainer}>
+                      {item.icon && (
+                        <Image source={item.icon} style={styles.image} />
+                      )}
+                      <SVText right body06 style={styles.barValueText}>
+                        {item.value}
+                      </SVText>
+                    </View>
+                    {item.description && (
+                      <SVText right caption01 style={styles.descriptionText}>
+                        {item.description}
+                      </SVText>
+                    )}
+                  </View>
+                </View>
+                {!isLastItem && <View style={styles.divider} />}
+              </>
+            );
+          })}
+        </View>
       </View>
     </ScrollView>
   );
@@ -63,6 +103,44 @@ const styles = StyleSheet.create({
   profileContainer: {
     height: AppStyles.scaleWidth(64),
     backgroundColor: AppStyles.color.lightGray02,
+  },
+  statusContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginVertical: AppStyles.scaleWidth(24),
+    borderWidth: AppStyles.scaleWidth(1),
+    borderRadius: AppStyles.scaleWidth(8),
+    borderColor: AppStyles.color.lightGray02,
+  },
+  barContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: AppStyles.scaleWidth(10),
+    marginHorizontal: AppStyles.scaleWidth(12),
+  },
+  divider: {
+    height: AppStyles.scaleWidth(1),
+    backgroundColor: AppStyles.color.lightGray02,
+  },
+  barValueText: {
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+  valueContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  image: {
+    marginRight: AppStyles.scaleWidth(3),
+    marginBottom: AppStyles.scaleWidth(2),
+  },
+  descriptionText: {
+    color: AppStyles.color.gray03,
   },
 });
 
