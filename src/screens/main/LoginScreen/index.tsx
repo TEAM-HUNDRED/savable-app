6060e180aa207bbc5792853c6b3e38bbd8b18d3f
yeongjunekoh <rhdudwns0@naver.com>
2023-10-05 23:45:45 +0900

    [FEATURE] Generate kakao Login ui

--- src/screens/main/LoginScreen/index.tsx
+++ src/screens/main/LoginScreen/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {HomeIcon} from '../../../assets/icons';
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import Images from '../../../assets/images';
+import Icon from '../../../assets/icons';
+import {AppStyles} from '../../../config';
+import SVText from '../../../components/common/SVText';
 
 function LoginScreen(): React.ReactElement {
   return (
     <View style={styles.container}>
-      <Text>1</Text>
-      {/* <HomeIcon /> */}
+      <ImageBackground source={Images.splash} style={styles.container} />
+      <View style={styles.LoginContainer}>
+        <TouchableOpacity style={styles.kakaoLogin} activeOpacity={1}>
+          <Image source={Icon.oauth.kakao} style={styles.icon} />
+          <SVText body05 style={styles.buttonText}>
+            {'카카오로 시작하기'}
+          </SVText>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,6 +31,35 @@ function LoginScreen(): React.ReactElement {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  LoginContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    width: '100%',
+    paddingHorizontal: AppStyles.scaleWidth(42),
+    bottom: AppStyles.scaleWidth(42),
+  },
+  kakaoLogin: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F7E600',
+    width: '100%',
+    height: AppStyles.scaleWidth(40),
+    paddingHorizontal: AppStyles.scaleWidth(12),
+    borderRadius: AppStyles.scaleWidth(6),
+    marginBottom: AppStyles.scaleWidth(20),
+  },
+  icon: {
+    width: AppStyles.scaleWidth(36),
+    height: AppStyles.scaleWidth(36),
+  },
+  buttonText: {
+    fontFamily: 'NotoSansKR-Bold',
+    marginTop: AppStyles.scaleWidth(2),
+    marginRight: AppStyles.scaleWidth(10),
   },
 });
 
