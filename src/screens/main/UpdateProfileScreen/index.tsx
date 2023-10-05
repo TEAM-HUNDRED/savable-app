1366aa3b7334428c2eacfb82b498ee9b310dbfdc
yeongjunekoh <rhdudwns0@naver.com>
2023-10-06 04:39:44 +0900

    [FEATURE] Generate Bottom Button Container, validation on nickName, phoneNumber

--- src/screens/main/UpdateProfileScreen/index.tsx
+++ src/screens/main/UpdateProfileScreen/index.tsx
@@ -13,19 +13,47 @@ import KakaoLogins, {getProfile} from '@react-native-seoul/kakao-login';
 
 import {AppStyles, MainScreenStackPropsList} from '../../../config';
 import SVText from '../../../components/common/SVText';
+import {SquareCheckIcon} from '../../../assets/icons';
 
 function UpdateProfileScreen(): React.ReactElement {
   const navigation =
     useNavigation<StackNavigationProp<MainScreenStackPropsList>>();
 
   const [profileData, setProfileData] = useState<KakaoLogins.KakaoProfile>();
+
   const [nickName, setNickName] = useState<string>('');
   const [phoneNumber, setPhoneNumber] = useState<string>('');
   const [verificationNumber, setVerificationNumber] = useState<string>('');
 
-  const [validatedNickName, setValidatedNickName] = useState<boolean>(true);
-  const [validatedNumber, setValidatedNumber] = useState<boolean>(true);
+  const [isValidatedNickName, setIsValidatedNickName] = useState<boolean>(true);
+  const [isValidatedNumber, setIsValidatedNumber] = useState<boolean>(true);
+  const [isValidatedPhoneNumber, setIsValidatedPhoneNumber] =
+    useState<boolean>(true);
   const [sentMessage, setSentMessage] = useState<boolean>(false);
+
+  const [isTermChecked, setIsTermChecked] = useState<boolean>(false);
+  const [isPrivacyChecked, setIsPrivacyChecked] = useState<boolean>(false);
+
+  const nameCheck = /^([가-힣]|[0-9]|[a-z]){2,10}$/;
+  const phoneCheck = /^010[0-9]{3,4}\d{4}$/;
+
+  const validateNickName = () => {
+    const nameValidation = nameCheck.test(nickName);
+
+    setIsValidatedNickName(nameValidation);
+  };
+
+  const validatePhoneNumber = () => {
+    const phoneValidation = phoneCheck.test(phoneNumber);
+
+    setIsValidatedPhoneNumber(phoneValidation);
+  };
+
+  const validateNumber = () => {
+    const phoneValidation = phoneCheck.test(verificationNumber);
+
+    return phoneValidation;
+  };
 
   const getKakaoProfile: () => Promise<KakaoLogins.KakaoProfile> = async () => {
     return await getProfile()
@@ -61,15 +89,16 @@ function UpdateProfileScreen(): React.ReactElement {
             placeholder={'닉네임을 입력해주세요'}
             style={styles.input}
             onChangeText={setNickName}
+            onBlur={validateNickName}
             value={nickName}
           />
-          {validatedNickName ? (
+          {isValidatedNickName ? (
             <SVText body06 style={styles.description}>
               {'닉네임을 2~10자로 입력해주세요'}
             </SVText>
           ) : (
             <SVText body06 style={styles.errorText}>
-              {'닉네임은 한글, 영문, 숫자만 입력 가능합니다.'}
+              {'닉네임을 한글, 영문, 숫자로 2~10자 입력해주세요'}
             </SVText>
           )}
         </View>
@@ -79,7 +108,9 @@ function UpdateProfileScreen(): React.ReactElement {
             <TextInput
               placeholder={'휴대폰 번호를 입력해주세요'}
               style={[styles.input, {flex: 1}]}
+              inputMode={'tel'}
               onChangeText={setPhoneNumber}
+              onBlur={validatePhoneNumber}
               value={phoneNumber}
             />
             {sentMessage ? (
@@ -108,7 +139,11 @@ function UpdateProfileScreen(): React.ReactElement {
               </TouchableOpacity>
             )}
           </View>
-          <SVText body06 style={styles.description}>
+          <SVText
+            body06
+            style={
+              isValidatedPhoneNumber ? styles.description : styles.errorText
+            }>
             {"'-' 없이 휴대폰 번호를 입력해주세요"}
           </SVText>
         </View>
@@ -118,14 +153,57 @@ function UpdateProfileScreen(): React.ReactElement {
             placeholder={'인증 번호를 입력해주세요'}
             style={styles.input}
             onChangeText={setVerificationNumber}
+            onBlur={validateNumber}
             value={verificationNumber}
+            inputMode={'numeric'}
           />
-          {!validatedNumber && (
+          {!isValidatedNumber && (
             <SVText body06 style={styles.errorText}>
               {'인증번호가 올바르지 않습니다.'}
             </SVText>
           )}
         </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <View style={styles.barContainer}>
+          <TouchableOpacity activeOpacity={0.8}>
+            <SVText
+              body05
+              style={
+                isTermChecked ? styles.highlightCheckText : styles.checkText
+              }>
+              {'[필수] 이용약관 '}
+            </SVText>
+          </TouchableOpacity>
+          <SquareCheckIcon
+            style={isTermChecked ? styles.highlightIcon : styles.icon}
+            hitSlop={{top: 5, bottom: 5, left: 5, right: 5}}
+            onPress={() => setIsTermChecked(prev => !prev)}
+          />
+        </View>
+        <View style={styles.barContainer}>
+          <TouchableOpacity activeOpacity={0.8}>
+            <SVText
+              body05
+              style={
+                isPrivacyChecked ? styles.highlightCheckText : styles.checkText
+              }>
+              {'[필수] 개인정보 처리 방침 '}
+            </SVText>
+          </TouchableOpacity>
+          <SquareCheckIcon
+            style={isPrivacyChecked ? styles.highlightIcon : styles.icon}
+            hitSlop={{top: 5, bottom: 5, left: 5, right: 5}}
+            onPress={() => setIsPrivacyChecked(prev => !prev)}
+          />
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={styles.finishButtonContainer}>
+          <SVText body03 style={styles.buttonText}>
+            {'완료'}
+          </SVText>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -148,7 +226,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     width: AppStyles.scaleWidth(84),
     height: AppStyles.scaleWidth(84),
-    marginVertical: AppStyles.scaleWidth(40),
+    marginVertical: AppStyles.scaleWidth(24),
   },
   image: {
     width: AppStyles.scaleWidth(84),
@@ -196,6 +274,43 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: AppStyles.color.white,
   },
+  bottomContainer: {
+    width: '100%',
+  },
+  finishButtonContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: AppStyles.scaleWidth(48),
+    marginTop: AppStyles.scaleWidth(12),
+    marginBottom: AppStyles.scaleWidth(24),
+    borderRadius: AppStyles.scaleWidth(8),
+    backgroundColor: AppStyles.color.mint04,
+  },
+  barContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: AppStyles.scaleWidth(12),
+  },
+  icon: {
+    color: AppStyles.color.gray03,
+    width: AppStyles.scaleWidth(18),
+    height: AppStyles.scaleWidth(18),
+  },
+  highlightIcon: {
+    color: AppStyles.color.mint04,
+    width: AppStyles.scaleWidth(18),
+    height: AppStyles.scaleWidth(18),
+  },
+  checkText: {
+    color: AppStyles.color.gray03,
+    borderBottomWidth: AppStyles.scaleWidth(1),
+    borderColor: AppStyles.color.gray03,
+  },
+  highlightCheckText: {},
 });
 
 export default React.memo(UpdateProfileScreen);
