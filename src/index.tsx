aa6c817e147da4da6995268f98955c46ebd05e36
yeongjunekoh <rhdudwns0@naver.com>
2023-10-06 17:58:17 +0900

    [FEATURE] Generate react-redux example

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+
 import PopUpProvider from './lib/context/PopUpContext';
 import ToastProvider from './lib/context/ToastContext';
+import {store} from './modules/redux/Store';
+
 import RouterScreen from './screens';
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <PopUpProvider>
-        <ToastProvider>
-          <RouterScreen />
-        </ToastProvider>
-      </PopUpProvider>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <PopUpProvider>
+          <ToastProvider>
+            <RouterScreen />
+          </ToastProvider>
+        </PopUpProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
