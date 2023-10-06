1ef8f913fd5a76a5d8797ee4b7cbd80ea59552dc
yeongjunekoh <rhdudwns0@naver.com>
2023-10-07 03:16:43 +0900

    [FEATURE] Generate challenge banner image

--- src/components/banner/MainBanner/index.tsx
+++ src/components/banner/MainBanner/index.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Image, Linking, StyleSheet, TouchableOpacity, View} from 'react-native';
+import Images from '../../../assets/images';
 import {AppStyles} from '../../../config';
 
 function MainBanner() {
-  return <View style={styles.headerContainer} />;
+  return (
+    <View style={styles.headerContainer}>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={styles.buttonContainer}
+        onPress={() => {
+          Linking.openURL(
+            'https://superb-nannyberry-327.notion.site/APP-d0e6b7c5ac9a4186b26fe2292a97b28d?pvs=4',
+          );
+        }}>
+        <Image source={Images.rewardEvent} style={styles.image} />
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   headerContainer: {
     width: '100%',
     height: AppStyles.scaleWidth(190),
-    backgroundColor: AppStyles.color.deepGray,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 });
 
