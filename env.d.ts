49d8396ea1f20c4eeb2b9a7b73ae32690a6b42f9
yeongjunekoh <rhdudwns0@naver.com>
2023-10-30 21:53:56 +0900

    [SETTING] git setting amplitude

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,5 @@
 declare module '@env' {
   export const API_URL: string;
+  export const SENTRY_URI: string;
+  export const AMPLITUDE_ID: string;
 }
--- index.js
+++ index.js
@@ -5,10 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './src';
 import {name as appName} from './app.json';
+import {SENTRY_URI, AMPLITUDE_ID} from '@env';
 import * as Sentry from '@sentry/react-native';
+import {init} from '@amplitude/analytics-react-native';
 
 Sentry.init({
-  dsn: 'https://3c53f610c2644b8567f4744f1a5c323c@o4505526964256768.ingest.sentry.io/4506106816757760',
+  dsn: SENTRY_URI,
 });
+
+init(AMPLITUDE_ID);
 
 AppRegistry.registerComponent(appName, () => App);
