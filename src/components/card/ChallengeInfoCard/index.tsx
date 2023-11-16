7d3b76795b6d90509468ee1a9d664250e357cd67
yeongjunekoh <rhdudwns0@naver.com>
2023-11-16 16:52:18 +0900

    [FIX] Fix event explanation

--- src/components/card/ChallengeInfoCard/index.tsx
+++ src/components/card/ChallengeInfoCard/index.tsx
@@ -26,16 +26,6 @@ function ChallengeInfoCard({
   return (
     <View style={styles.container}>
       <SVText color={AppStyles.color.deepGray}>{explanation}</SVText>
-      <View style={styles.explanationContainer}>
-        <View style={styles.textContainer}>
-          <SVText color={AppStyles.color.black}>{'📌 '}</SVText>
-          <SVText color={AppStyles.color.deepGray}>
-            {
-              '기프티콘 100% 지급 EVENT 진행중!\n목표 3회 이상 설정 후 챌린지 성공하면 완료 🤗\n자세한 사항은 홈페이지 배너를 클릭해주세요'
-            }
-          </SVText>
-        </View>
-      </View>
       <View style={styles.infoContainer}>
         <ChallengeInfoBar
           title={'1회 인증 시'}
@@ -66,18 +56,7 @@ const styles = StyleSheet.create({
   titleText: {
     marginVertical: AppStyles.scaleWidth(4),
   },
-  explanationContainer: {
-    flexDirection: 'column',
-    borderRadius: AppStyles.scaleWidth(8),
-    padding: AppStyles.scaleWidth(8),
-    paddingHorizontal: AppStyles.scaleWidth(12),
-    marginTop: AppStyles.scaleWidth(12),
-    width: '100%',
-    backgroundColor: AppStyles.color.lightGray02,
-  },
-  textContainer: {
-    flexDirection: 'row',
-  },
+
   buttonContainer: {
     width: '100%',
     alignItems: 'center',
